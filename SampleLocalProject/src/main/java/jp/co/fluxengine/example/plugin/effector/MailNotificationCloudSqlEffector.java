a2150a0076a076ef48907efd71bbb2fd967db152
ma.dengcheng@fluxengine.co.jp <ma.dengcheng@fluxengine.co.jp>
2019-07-04 20:30:01 +0900

    effector2 テスト index

--- SampleLocalProject/src/main/java/jp/co/fluxengine/example/plugin/effector/MailNotificationCloudSqlEffector.java
+++ SampleLocalProject/src/main/java/jp/co/fluxengine/example/plugin/effector/MailNotificationCloudSqlEffector.java
@@ -43,7 +43,7 @@ public class MailNotificationCloudSqlEffector {
 
             // PreparedStatements can be more efficient and project against injections.
             PreparedStatement voteStmt = conn.prepareStatement(
-                "INSERT INTO effector (userid, message, createTime) VALUES (?, ? ,? );");
+                "INSERT INTO effector2 (userid, message, createTime, m1 , m2, m3, m4, m5, m6, m7,m8) VALUES (?, ? ,? ,'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7','a8');");
 
             voteStmt.setString(1, userId);
             voteStmt.setString(2, message);
@@ -63,4 +63,18 @@ public class MailNotificationCloudSqlEffector {
 *
 CREATE TABLE effector (userid VARCHAR(255), message VARCHAR(255),createTime timestamp ,
 messageId INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(messageId));
+
+CREATE TABLE effector2(userid VARCHAR(255), message VARCHAR(255),createTime timestamp ,
+messageId INT NOT NULL AUTO_INCREMENT, m1 VARCHAR(255), m2 VARCHAR(255), m3 VARCHAR(255),
+m4 VARCHAR(255), m5 VARCHAR(255), m6 VARCHAR(255), m7 VARCHAR(255), m8 VARCHAR(255),PRIMARY KEY(messageId) ,
+  KEY `FK_check_results_0` (`m1`),
+  KEY `FK_check_results_1` (`m2`),
+  KEY `FK_check_results_2` (`m3`),
+  KEY `FK_check_results_3` (`m4`),
+  KEY `FK_check_results_4` (`m5`),
+  KEY `FK_check_results_5` (`m6`),
+  KEY `FK_check_results_6` (`m7`),
+  KEY `FK_check_results_7` (`m8`)
+) ENGINE=InnoDB AUTO_INCREMENT=15939991 DEFAULT CHARSET=utf8mb4
+
 */
